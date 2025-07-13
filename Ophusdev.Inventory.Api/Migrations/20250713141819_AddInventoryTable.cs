e17dc0d10f88be040f47a19f4995c333039ddae8
OphusDev <53277151+ophusdev@users.noreply.github.com>
2025-07-13 16:37:56 +0200

    add migration

---
+++ Ophusdev.Inventory.Api/Migrations/20250713141819_AddInventoryTable.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Ophusdev.Inventory.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddInventoryTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Rooms",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PricePerNight = table.Column<decimal>(type: "decimal(5,2)", precision: 5, scale: 2, nullable: false),
+                    MaxCapacity = table.Column<int>(type: "int", nullable: false),
+                    RoomCategory = table.Column<int>(type: "int", nullable: false),
+                    IsAvailable = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Rooms", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Rooms",
+                columns: new[] { "Id", "IsAvailable", "MaxCapacity", "Name", "PricePerNight", "RoomCategory" },
+                values: new object[,]
+                {
+                    { 1, true, 4, "Roma", 180m, 0 },
+                    { 2, true, 4, "Parigi", 220m, 1 },
+                    { 3, true, 3, "New York", 440m, 2 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Rooms");
+        }
+    }
+}
---
+++ Ophusdev.Inventory.Repository/InventoryDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Inventory.Repository.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Inventory.Repository;
+
+public class InventoryDbContext(DbContextOptions<InventoryDbContext> dbContextOptions) : DbContext(dbContextOptions)
+{
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Room>().HasKey(x => x.Id);
+        modelBuilder.Entity<Room>().Property(e => e.Id).ValueGeneratedOnAdd();
+
+        modelBuilder.Entity<Room>().HasData(
+            new Room { Id = 1, Name = "Roma", MaxCapacity = 4, PricePerNight = 180, RoomCategory = 0, IsAvailable = true },
+            new Room { Id = 2, Name = "Parigi", MaxCapacity = 4, PricePerNight = 220, RoomCategory = 1, IsAvailable = true },
+            new Room { Id = 3, Name = "New York", MaxCapacity = 3, PricePerNight = 440, RoomCategory = 2, IsAvailable = true }
+        );
+    }
+
+    public DbSet<Room> Rooms { get; set; }
+}
